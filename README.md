aa7afd95ee89515d1db1ba7deac053e006a2836d
Seth Weidman <sethhweidman@gmail.com>
2017-08-15 17:50:34 -0500

    Full restaurant menu app

--- README.md
+++ README.md
@@ -2,4 +2,6 @@ Running `python3 database_setup.py` sets up the database, and running `python3 l
 
 Finally, `python3 project.py` starts the app! Then, navigate to `http://0.0.0.0:5000/restaurants/1/` to see the app in action.
 
-Clicking "Create New Item" will pull up the "create new item" page. From that page, you can enter item information, hit submit, and see the results. Similarly for "Edit Item".
+Clicking "Create New Item" will pull up the "create new item" page. From that page, you can enter item information, hit submit, and see the results. Similarly for "Edit Item" and "Delete Item".
+
+Finally, you can go to `http://0.0.0.0:5000/restaurants/1/menu/2/JSON` to see the JSON for just one menu item, or go to `http://0.0.0.0:5000/restaurants/1/JSON` to see the JSON for an entire menu.
--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -19,7 +19,20 @@ def restaurantMenu(restaurant_id):
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     return render_template('menu.html', restaurant=restaurant, items=items)
 
-# Task 1: Create route for newMenuItem function here
+
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurantMenuJSON(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    items = session.query(MenuItem).filter_by(
+        restaurant_id=restaurant_id).all()
+    return jsonify(MenuItems=[i.serialize for i in items])
+
+
+# ADD JSON ENDPOINT HERE
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+def menuItemJSON(restaurant_id, menu_id):
+    menuItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    return jsonify(MenuItem=menuItem.serialize)
 
 
 @app.route('/restaurants/<int:restaurant_id>/new', methods=['GET', 'POST'])
